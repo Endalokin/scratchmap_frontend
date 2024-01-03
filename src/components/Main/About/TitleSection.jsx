6babaabb5c43df0bacfbb4afddd971abebdf37ec
Nora Westphal <tupfen@hotmail.de>
2024-01-03 18:12:18 +0100

    moved assets to public folder

--- src/components/Main/About/TitleSection.jsx
+++ src/components/Main/About/TitleSection.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 export default function TitleSection() {
     return (
         <div className="grid-3">
-            <div><img src="./src/assets/header_specht.jpg" alt="" /></div>
-            <div><img src="./src/assets/header_seerose.png" alt="" /></div>
-            <div><img src="./src/assets/header_seerose.png" alt="" /></div>
-            <div><img src="./src/assets/header_specht.jpg" alt="" /></div>
+            <div><img src="./assets/header_specht.jpg" alt="" /></div>
+            <div><img src="./assets/header_seerose.png" alt="" /></div>
+            <div><img src="./assets/header_seerose.png" alt="" /></div>
+            <div><img src="./assets/header_specht.jpg" alt="" /></div>
             <div><h1>Scratch Map</h1></div>
-            <div><img src="./src/assets/header_seerose.png" alt="" /></div>
-            <div><img src="./src/assets/header_specht.jpg" alt="" /></div>
-            <div><img src="./src/assets/header_seerose.png" alt="" /></div>
-            <div><img src="./src/assets/header_specht.jpg" alt="" /></div>
+            <div><img src="./assets/header_seerose.png" alt="" /></div>
+            <div><img src="./assets/header_specht.jpg" alt="" /></div>
+            <div><img src="./assets/header_seerose.png" alt="" /></div>
+            <div><img src="./assets/header_specht.jpg" alt="" /></div>
         </div>
     )
 }
--- src/components/Main/Footprint/CompensationTree.jsx
+++ src/components/Main/Footprint/CompensationTree.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CompensationTree({ compensatedTrip }) {
     return (
         <>
-            {compensatedTrip ? <img src="./src/assets/democontent_tree_living.png" alt="living tree" /> : <img src="./src/assets/democontent_tree_dead.png" alt="dead tree" />}
+            {compensatedTrip ? <img src="./assets/democontent_tree_living.png" alt="living tree" /> : <img src="./public/assets/democontent_tree_dead.png" alt="dead tree" />}
         </>
     )
 }
