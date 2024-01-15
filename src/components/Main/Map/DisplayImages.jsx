86bdfd2cc604c8c3793eaf7e1118eddad8b78dbf
Nora Westphal <tupfen@hotmail.de>
2024-01-15 21:32:52 +0100

    direct link from map items to detail page, minor design improvements

--- src/components/Main/Map/DisplayImages.jsx
+++ src/components/Main/Map/DisplayImages.jsx
@@ -5,27 +5,26 @@ import filteredArray from './filteredArray'
 import { useNavigate } from 'react-router-dom';
 
 export default function DisplayImages({ experiences, displaySeasons, displayDaytime }) {
-    const {pathname} = window.location
+    const { pathname } = window.location
+
+    const navigate = useNavigate()
 
     let currentArray = filteredArray(experiences, displaySeasons, displayDaytime)
-    return (
-        <>
-            {currentArray?.reverse().map((e,index) => {
-                const htmlValue = pathname != "/map" ? `<p style="text-align: center">Stop ${index + 1}</p>` : `<p></p>`
 
-                return <Marker key={e.id}  position={[e.location.lat, e.location.lon]} icon={new L.DivIcon({ html: `<img src="${e?.imgUrl}?fm=webp&w=100"/>${htmlValue}`, className: "polaroid", iconSize: [100, ] })} >
-                    <Popup>
-                        {e.image.sys.id} <br />
-                        <a href={e.imgUrl} target="_blank" rel="noreferrer">{e.imgUrl}</a>
-                    </Popup>
-                    {<Tooltip sticky direction="bottom">
-                        <p><span style={{color: e.imgAccentColour, fontWeight: "bold"}}>{e?.name}</span> <br />
-                            {new Date(e?.date).toLocaleDateString()},
-                            {new Date(e?.date).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })} Uhr
-                        </p>
-                    </Tooltip>}
-                </Marker>
-            })}
-        </>
-    )
+return (
+    <>
+        {currentArray?.reverse().map((e, index) => {
+            const htmlValue = pathname != "/map" ? `<p style="text-align: center">Stop ${index + 1}</p>` : `<p></p>`
+
+            return <Marker key={e.id} position={[e.location.lat, e.location.lon]} icon={new L.DivIcon({ html: `<img src="${e?.imgUrl}?fm=webp&w=100"/>${htmlValue}`, className: "polaroid", iconSize: [100,] })} eventHandlers={{click: () => navigate(`/trip/${e?.trip.sys.id}/${e?.id}`)}} >
+                {<Tooltip sticky direction="bottom">
+                    <p><span style={{ color: e.imgAccentColour, fontWeight: "bold" }}>{e?.name}</span> <br />
+                        {new Date(e?.date).toLocaleDateString()},
+                        {new Date(e?.date).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })} Uhr
+                    </p>
+                </Tooltip>}
+            </Marker>
+        })}
+    </>
+)
 }
