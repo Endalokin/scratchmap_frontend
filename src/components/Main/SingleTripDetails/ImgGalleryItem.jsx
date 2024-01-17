4a40fb4dfb831ca6c984e5fa6aca5c9c1825e719
Nora Westphal <tupfen@hotmail.de>
2024-01-17 10:21:52 +0100

    provide different image sizes for title section

--- src/components/Main/SingleTripDetails/ImgGalleryItem.jsx
+++ src/components/Main/SingleTripDetails/ImgGalleryItem.jsx
@@ -3,10 +3,6 @@ import { Link } from 'react-router-dom'
 
 export default function ImgGalleryItem({ singleTripExperience, widthSize }) {
 
-    console.log(Math.floor(window.innerWidth))
-    console.log(Math.floor(window.innerHeight * 0.6))
-
-
     return (
         <div className='polaroid'>
             <Link to={`${window.location.origin}/trip/${singleTripExperience?.trip.sys.id}/${singleTripExperience?.id}`}><img key={singleTripExperience?.id} src={`${singleTripExperience?.imgUrl}?fm=webp&w=${widthSize}`} /></Link>
