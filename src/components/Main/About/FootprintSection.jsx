812d8afa7e2b1a6831218d9ba1cf6eb4fcb8e7df
Nora Westphal <tupfen@hotmail.de>
2024-02-16 18:11:11 +0100

    current version is shown in footer

--- src/components/Main/About/FootprintSection.jsx
+++ src/components/Main/About/FootprintSection.jsx
@@ -6,7 +6,7 @@ export default function FootprintSection({trips}) {
     return (
         <>
             <h2>Got to know your footprint</h2>
-            <p style={{maxWidth: "880px"}}>All this is fun, but unfortunately travelling is usually bad for the environment</p>
+            <p style={{maxWidth: "880px"}}>All this is fun, but unfortunately travelling is usually bad for the environment.</p>
             <p style={{maxWidth: "880px"}}>Receive a realistic insight of how your trips influence the environment and learn about different options <span style={{whiteSpace: "nowrap"}}>for your next journey.</span> </p>
             <Link to="/footprint"><button className="notching">Go to footprint overview</button></Link>
             <div>{trips?.map((trip) => <CompensationTree key={`about-tree-${trip.id}`} compensatedTrip={trip?.footprint?.compensated} />)}</div>
