ebca311694b9dc7d7628c3e6d1bf6c26d6998f50
Nora Westphal <tupfen@hotmail.de>
2024-04-17 17:50:27 +0200

    altitude is read and track segments displayed in red or blue depending if it is up- or downhill

--- src/components/Main/SingleTripDetails/EditTrip.jsx
+++ src/components/Main/SingleTripDetails/EditTrip.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
-import SeasonFilter from './SeasonFilter'
-import ClockFilter from './ClockFilter'
+import React, { useState } from 'react'
+import { parseGpxFile } from '../../../utils/parseGPXFile';
+import fetchData from '../../../utils/fetchAPI';
 
-export default function EditTrip({ toggleFilterVisibility }) {
+export default function EditTrip({ singleTripDetails, toggleFilterVisibility }) {
+
+    const { VITE_SERVER_URL } = import.meta.env;
+    const [valueTracks, setValueTracks] = useState()
+
+    async function trackUpload(e) {
+        e.preventDefault()
+        console.log("A track is loading")
+        console.log("It is ", valueTracks[0])
+        const reader = new FileReader();
+
+        reader.addEventListener(
+            "load",
+            async () => {
+                // this will then display a text file
+                const parseGpxData = await parseGpxFile(reader.result);
+                console.log("I transformed it to: ", parseGpxData)
+
+                let body = {
+                    name: parseGpxData[0]?.name,
+                    path: parseGpxData[0]?.positions
+                }
+                console.log(body)
+                const POST_TRACK_URL = `${VITE_SERVER_URL}/trips/addTrack/${singleTripDetails?.id}`
+                console.log(POST_TRACK_URL)
+                fetchData(POST_TRACK_URL, (data) => {
+                    console.log(data)
+                }, "POST", body)
+            },
+            false,
+        );
+
+        if (valueTracks[0]) {
+            reader.readAsText(valueTracks[0]);
+        }
+/*         const parseGpxData = await parseGpxFile(valueTracks[0].name);
+        console.log(parseGpxData) 
+ */    }
+    function handleChange(e) {
+        let inputId = e.target.id;
+        if (inputId == "add_gpx_track") {
+            setValueTracks(e.target.files);
+        }
+    }
 
     return (
         <>
@@ -10,6 +53,10 @@ export default function EditTrip({ toggleFilterVisibility }) {
             <h2>Edit</h2>
             <div>
                 <h3>Tracks</h3>
+                <form action="" onSubmit={trackUpload}>
+                    <input type="file" name="add_gpx_track" id="add_gpx_track" accept=".gpx" onChange={handleChange} multiple /><br />
+                    <button>Add tracks</button>
+                </form>
             </div>
         </>
     )
