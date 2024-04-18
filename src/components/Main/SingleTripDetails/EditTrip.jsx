680484cdcb1c313fe4bb992cd213e90123e9c007
Nora Westphal <tupfen@hotmail.de>
2024-04-18 11:12:13 +0200

    delete function for tracks, display of deleted and added tracks

--- src/components/Main/SingleTripDetails/EditTrip.jsx
+++ src/components/Main/SingleTripDetails/EditTrip.jsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react'
 import { parseGpxFile } from '../../../utils/parseGPXFile';
 import fetchData from '../../../utils/fetchAPI';
 import RowPerTrack from './EditOptions/RowPerTrack';
+import { useOutletContext } from 'react-router-dom'
 
 export default function EditTrip({ singleTripDetails, toggleFilterVisibility }) {
 
     const { VITE_SERVER_URL } = import.meta.env;
-    const [valueTracks, setValueTracks] = useState()
+    const [newTrack, setNewTrack] = useState()
     const [isOpen, setIsOpen] = useState(false)
+    const [activeTrack, setActiveTrack] = useState()
+    const [adder, setAdder] = useState(<button>Add track</button>)
+    const [, , , setTrips, user ] = useOutletContext();
 
     async function trackUpload(e) {
         e.preventDefault()
-        console.log("A track is loading")
-        console.log("It is ", valueTracks[0])
+        setAdder(<p className="rotating">{'\u27F3'}</p>)
         const reader = new FileReader();
 
         reader.addEventListener(
@@ -26,28 +29,45 @@ export default function EditTrip({ singleTripDetails, toggleFilterVisibility }) 
                 }
                 const POST_TRACK_URL = `${VITE_SERVER_URL}/trips/addTrack/${singleTripDetails?.id}`
                 fetchData(POST_TRACK_URL, (data) => {
-                    console.log(data)
+                    const TRIPS_URL = `${VITE_SERVER_URL}/trips`
+                    let controller = new AbortController();
+                    fetchData(TRIPS_URL, (data) => {
+                        setTrips(data)
+                      }, "GET", null, user?.token, controller)
+                    e.target.reset()
+                    setAdder(<button>Add track</button>)
                 }, "POST", body)
             },
             false,
         );
 
-        if (valueTracks[0]) {
-            reader.readAsText(valueTracks[0]);
+        if (newTrack[0]) {
+            reader.readAsText(newTrack[0]);
         }
     }
 
     function handleChange(e) {
         let inputId = e.target.id;
         if (inputId == "add_gpx_track") {
-            setValueTracks(e.target.files);
+            setNewTrack(e.target.files);
         }
     }
 
-    function deleteNo () {
+    function deleteNo(e) {
+        e.preventDefault()
         setIsOpen(false)
     }
-    function deleteYes () {
+    function deleteYes(e) {
+        e.preventDefault()
+
+        setTrips(prev => {
+            prev.find((p) => p.id == singleTripDetails.id).tracks = singleTripDetails.tracks.filter(track => track != activeTrack)
+            return [...prev]
+        })
+
+        const DELETE_URL = `${VITE_SERVER_URL}/trips/deleteTrack/${activeTrack.id}`
+        fetchData(DELETE_URL, (data) => {
+        }, "DELETE")
         setIsOpen(false)
     }
 
@@ -58,24 +78,24 @@ export default function EditTrip({ singleTripDetails, toggleFilterVisibility }) 
             <div>
                 <h3>Tracks</h3>
                 <ul>
-                    {singleTripDetails?.tracks?.map(t => <RowPerTrack key={t.id} t={t} setIsOpen={setIsOpen} /> )}
+                    {singleTripDetails?.tracks?.map(t => <RowPerTrack key={t.id} t={t} setIsOpen={setIsOpen} setActiveTrack={setActiveTrack}/>)}
                 </ul>
                 <form action="" onSubmit={trackUpload}>
                     <input type="file" name="add_gpx_track" id="add_gpx_track" accept=".gpx" onChange={handleChange} /* multiple */ /><br />
-                    <button>Add track</button>
+                    {adder}
                 </form>
             </div>
-            {isOpen && 
-            <>
-                <div className='modal' id="compensationModal" >
-                Do you want to permanently delete this track?
-                <div>
-                    <button className="ribbon ribbon-secondary" onClick={deleteNo}>No</button>
-                    <button className="notching" onClick={deleteYes}>Yes</button>
-                </div>
-                </div>
-                <div className="modal-back"></div>
-            </>
+            {isOpen &&
+                <>
+                    <div className='modal' id="compensationModal" >
+                        Do you want to permanently delete track {activeTrack.name}?
+                        <div>
+                            <button className="ribbon ribbon-secondary" onClick={deleteNo}>No</button>
+                            <button className="notching" onClick={deleteYes}>Yes</button>
+                        </div>
+                    </div>
+                    <div className="modal-back"></div>
+                </>
             }
         </>
     )
